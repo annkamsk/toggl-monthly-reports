8c2bd9b9fa04e74d9b5c6a1827fafd02ada40202
ak <an.kramarska@gmail.com>
2022-02-08 23:45:20 +0100

    Change name for detailed report

--- toggl.py
+++ toggl.py
@@ -13,7 +13,11 @@ from requests.auth import HTTPBasicAuth
 
 
 USERNAME = ""
-PASSWORD = ""
+PASSWORD = ''
+
+HANDLE = ""
+COMPANY = ""
+WORKSPACE_ID = ""
 
 BASE_URL = "https://api.track.toggl.com"
 
@@ -21,6 +25,11 @@ BASE_URL = "https://api.track.toggl.com"
 class ReportType(Enum):
     SUM = "summary"
     DET = "details"
+
+    def filename(self) -> str:
+        if self == ReportType.DET:
+            return "time_entries"
+        return f"{self.value}_report"
 
 
 class FileExtension(Enum):
@@ -33,6 +42,9 @@ class FileExtension(Enum):
 class MonthRange:
     _start: datetime
     _end: datetime
+    days: int
+    month: int
+    year: int
 
     @property
     def start(self) -> str:
@@ -42,80 +54,82 @@ class MonthRange:
     def end(self) -> str:
         return self._end.strftime("%Y-%m-%d")
 
-    def __init__(self, first_day: Optional[datetime]) -> None:
-        if not first_day:
-            self._end = (datetime.now().replace(day=1) - timedelta(days=1))
-            self._start = self._end.replace(day=1)
-            return
+    def __init__(self, month: Optional[int], year: Optional[int]) -> None:
+        last_day_of_prev_month = datetime.now().replace(day=1) - timedelta(days=1)
+        self.month = month or last_day_of_prev_month.month
+        self.year = year or last_day_of_prev_month.year
 
-        self._start = first_day
-        _, days_count = calendar.monthrange(self._start.year, self._start.month)
-        self._end = self._start + timedelta(days=days_count)
+        self._start = datetime(self.year, self.month, 1)
+        _, self.days = calendar.monthrange(self._start.year, self._start.month)
+        self._end = self._start + timedelta(days=self.days - 1)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Downloads your monthly toggl reports.")
-
-    def type_date(value):
-        if not value:
-            return value
-
-        try:
-            dvalue = datetime.strptime(value, "%Y-%m-%d")
-        except ValueError:
-            raise argparse.ArgumentTypeError(f"{value} is an invalid date value. Must have YYYY-mm-dd format.")
-        return dvalue
-
-    parser.add_argument("--date", metavar="D",
-                        help="First day of the month in YYYY-mm-dd format, default: whole previous month",
-                        default=None, type=type_date)
+    parser = argparse.ArgumentParser(
+        description="Downloads your monthly toggl reports."
+    )
+    parser.add_argument(
+        "--month",
+        metavar="m",
+        help="Month as integer number, default: previous month",
+        default=None,
+        type=int,
+    )
+    parser.add_argument(
+        "--year",
+        metavar="y",
+        help="Year in yyyy format, default: year of the previous month.",
+        default=None,
+        type=int,
+    )
     return parser.parse_args()
 
 
 def run(args: argparse.Namespace):
-    month_range = MonthRange(args.date)
+    month_range = MonthRange(args.month, args.year)
     api_token = authenticate(USERNAME, PASSWORD)
-    check_correctness(api_token, month_range)
+
+    response = get_report(api_token, ReportType.DET, FileExtension.NONE, month_range)
+    entries = response.json()["data"]
+
+    check_correctness(entries)
     save_report(api_token, ReportType.SUM, FileExtension.PDF, month_range)
     save_report(api_token, ReportType.DET, FileExtension.PDF, month_range)
     save_report(api_token, ReportType.DET, FileExtension.CSV, month_range)
 
 
 def authenticate(user: str, password: str) -> str:
-    response = requests.get(
-        f"{BASE_URL}/api/v8/me", auth=HTTPBasicAuth(user, password)
-    )
+    response = requests.get(f"{BASE_URL}/api/v8/me", auth=HTTPBasicAuth(user, password))
     response.raise_for_status()
     return response.json()["data"]["api_token"]
 
 
-def check_correctness(api_token: str, month_range: MonthRange):
-    response = get_report(api_token, ReportType.DET, FileExtension.NONE, month_range)
-
-    entries = response.json()["data"]
+def check_correctness(entries: List) -> None:
     try:
         check_if_empty(entries)
         check_reasonable_time(entries)
         check_if_overlapping(entries)
     except Exception as e:
-        logging.warning(
-            f"Raised exception {e} while checking for correctness."
-        )
+        logging.warning(f"Raised exception {e} while checking for correctness.")
 
 
 def get_report(
-    api_token: str, report_type: ReportType, file_ext: FileExtension, month_range: MonthRange
+    api_token: str,
+    report_type: ReportType,
+    file_ext: FileExtension,
+    month_range: MonthRange,
 ) -> Response:
     # docs: https://github.com/toggl/toggl_api_docs/blob/master/reports.md
-
     params = {
         "since": month_range.start,
         "until": month_range.end,
-        "workspace_id": "4014667",
-        "user_agent": "ak@qed.ai",
+        "workspace_id": WORKSPACE_ID,
+        "user_agent": USERNAME,
     }
-    url = f"{BASE_URL}/reports/api/v2/" \
-          f"{report_type.value}{'.' if file_ext != FileExtension.NONE else ''}{file_ext.value}"
+    url = (
+        f"{BASE_URL}/reports/api/v2/"
+        f"{report_type.value}{'.' if file_ext != FileExtension.NONE else ''}{file_ext.value}"
+    )
 
     response = requests.get(
         url, params=params, auth=HTTPBasicAuth(api_token, "api_token")
@@ -146,9 +160,7 @@ def check_reasonable_time(entries: List):
 
 
 def check_if_overlapping(entries: List):
-    named_interval = namedtuple(
-        "named_interval", ["start", "end", "description"]
-    )
+    named_interval = namedtuple("named_interval", ["start", "end", "description"])
     time_intervals = [
         named_interval(
             datetime.fromisoformat(entry["start"]),
@@ -166,10 +178,13 @@ def check_if_overlapping(entries: List):
 
 
 def save_report(
-    api_token: str, report_type: ReportType, file_ext: FileExtension, month_range: MonthRange
+    api_token: str,
+    report_type: ReportType,
+    file_ext: FileExtension,
+    month_range: MonthRange,
 ) -> None:
     response = get_report(api_token, report_type, file_ext, month_range)
-    filename = f"{report_type.value}_report_{month_range.start}_to_{month_range.end}.{file_ext.value}"
+    filename = f"{COMPANY}_{HANDLE}_{report_type.filename()}_{month_range.start}_to_{month_range.end}.{file_ext.value}"
     with open(filename, "wb+") as report:
         report.write(response.content)
 
